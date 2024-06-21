ec3cf817b6857fb3da7057395718f173cca02ce6
leedrum <levantungat@gmail.com>
2024-06-22 01:41:50 +0700

    config db, test, write simple CRUD

---
+++ util/random.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"math/rand"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func RandomInt(min, max int64) int64 {
+	return rand.Int63n(max-min+1) + min
+}
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(b)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "VND"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
